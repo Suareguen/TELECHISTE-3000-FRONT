9effb6e31c00cc57dc106c3e0b9e1cb2630a0720
Suareguen <adrian.suarez.monzon@gmail.com>
2024-06-06 19:51:51 +0100

    .

--- telechiste/src/components/Header/Header.jsx
+++ telechiste/src/components/Header/Header.jsx
@@ -3,24 +3,15 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import SideBar from '../SideBar/SideBAr';
 
 export default function Header() {
   return (
     <Box>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
+          <SideBar />
+          <Typography variant="h6" component="div">
             TELECHISTE 3000
           </Typography>
           <Button color="inherit">Login</Button>
