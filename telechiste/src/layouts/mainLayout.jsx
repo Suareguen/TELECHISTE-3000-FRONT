e9986254d79be26e923ac18203d1139b7d3ea154
Suareguen <adrian.suarez.monzon@gmail.com>
2024-06-06 19:15:01 +0100

    header and footer created

--- telechiste/src/layouts/mainLayout.jsx
+++ telechiste/src/layouts/mainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
 
 function Root() {
   return (
     <div>
-        <h1>Header</h1>
+        <Header />
         <Outlet />
-        <h1>Footer</h1>
+        <Footer />
     </div>
   )
 }
